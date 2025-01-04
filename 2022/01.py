89d6ef4445e9bee73aaf75fcb9892b28dcbc282e
Sem <sem@ie-design.nl>
2025-01-04 16:02:55 +0100

    both parts work

--- 2022/01.py
+++ 2022/01.py
@@ -2,17 +2,10 @@ import io, os
 import time
 from collections import Counter, defaultdict, deque
 
-# file = defaultdict(None, {'file', 'amountlines', 'lengthlines'})
-
 def main():
-	# readfile("T1")
-	global file; file = readfile("M")
-	part1()
-	part2()
-
-def part1():
+	global file; file = readfile("T1")
 	j = 0
-	maxssum = 0
+	maxssum = []
 	while(True):
 		ssum = 0
 		while(len(file[j])):
@@ -20,35 +13,24 @@ def part1():
 			j += 1
 			if(j >= len(file)):
 				break
-		maxssum = max(maxssum, ssum)
+		maxssum.append(ssum)
 		j += 1
 		if(j >= len(file)):
 			break
-	print(maxssum)
-
-def part2():
-	pass
-
-
+	maxssum.sort(reverse = True)
+	print("Part 1:", maxssum[0])
+	print("Part 2:", maxssum[0] + maxssum[1] + maxssum[2])
 
 def readfile(what = "M"):
 	day= __file__.split(os.sep)[-1].replace('.py', '')
-
 	dir = os.path.dirname(os.path.realpath(__file__)) + os.sep
-
 	tmp = dir + "txt" + os.sep + day + ".txt"
-	if(what == "M"):
-		with io.open(tmp, "r", encoding="utf-8") as data:
-			running_test_data = False
-
+	
 	if(what == "T1"):
 		tmp = tmp.replace('.', '.test1.')
-		with io.open(tmp, "r", encoding="utf-8") as data:
-			running_test_data = True
-
+		
 	if(what == "T2"):
-		with io.open(tmp.replace('.','.test2.'), "r", encoding="utf-8") as data:
-			running_test_data = True
+		tmp = tmp.replace('.', '.test2.')
 		
 	with io.open(tmp, "r", encoding="utf-8") as data:
 		print(tmp)
