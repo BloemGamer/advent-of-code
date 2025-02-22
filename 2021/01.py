6d12a8b1fcb6974d7edac315388e2738fe067e70
Bloem <sem@ie-design.nl>
2025-02-22 12:06:26 +0100

    part 2 works

--- 2021/01.py
+++ 2021/01.py
@@ -4,17 +4,14 @@ import itertools
 from collections import Counter, defaultdict, deque
 from adventofcode import *
 
-
 def main() -> None:
 	global file
 	numbers = list()
-	answer = 0
-	file = fix_file(__file__, "T1")
-	for f in file:
-		numbers.append(int(f))
-	diff = [b - a for a, b in itertools.pairwise(numbers)]
-	for d in diff:
-		if(d >= 0):
-			answer += 1
-	print(answer)
+	file = fix_file(__file__, "M")
+	numbers = [int(f) for f in file if f != ""]
+	print(sum([(b - a) >= 0 for a, b in itertools.pairwise(numbers)])) # part 1 
+	s = [(numbers[i] + numbers[i + 1] + numbers[i + 2])for i in range(0, len(numbers) - 2)]
+	print(sum([(b - a) > 0 for a, b in itertools.pairwise(s)])) # part 2
+
+
 main()
