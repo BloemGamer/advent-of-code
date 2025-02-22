a3434dbbb42e2a34c1b1f89a3c0dd66b827b506d
Bloem <sem@ie-design.nl>
2025-02-22 12:11:40 +0100

    made the code shorter

--- 2021/01.py
+++ 2021/01.py
@@ -5,13 +5,9 @@ from collections import Counter, defaultdict, deque
 from adventofcode import *
 
 def main() -> None:
-	global file
-	numbers = list()
-	file = fix_file(__file__, "M")
-	numbers = [int(f) for f in file if f != ""]
+	numbers = [int(f) for f in fix_file(__file__, "M") if f != ""]
 	print(sum([(b - a) >= 0 for a, b in itertools.pairwise(numbers)])) # part 1 
-	s = [(numbers[i] + numbers[i + 1] + numbers[i + 2])for i in range(0, len(numbers) - 2)]
-	print(sum([(b - a) > 0 for a, b in itertools.pairwise(s)])) # part 2
+	print(sum([(b - a) > 0 for a, b in itertools.pairwise([sum((numbers[i:i + 3]))for i in range(0, len(numbers) - 2)])])) # part 2
 
 
 main()
