09fc30c7bd3f7245fded8d1ddb9fac1e473b07a1
Bloem <sem@ie-design.nl>
2025-09-29 11:32:54 +0200

    changed how to code is layedout and removed the eprint calls

--- libs/rs/proc/src/lib.rs
+++ libs/rs/proc/src/lib.rs
@@ -86,7 +86,8 @@ pub fn insert_here(_input: TokenStream) -> TokenStream
 /// - Adds a new parameter: map: &[T] where T: AsRef<[char]>
 /// - Replaces all insert_here!(code) with the code inside the macro call
 #[proc_macro_attribute]
-pub fn add_show(_args: TokenStream, input: TokenStream) -> TokenStream {
+pub fn add_show(_args: TokenStream, input: TokenStream) -> TokenStream
+{
     let input_fn = parse_macro_input!(input as ItemFn);
     let mut original_fn = input_fn.clone();
     
@@ -106,8 +107,10 @@ pub fn add_show(_args: TokenStream, input: TokenStream) -> TokenStream {
     modified_fn.sig.ident = new_name;
     
     // Check if 'T' generic parameter already exists
-    let has_t_generic = modified_fn.sig.generics.params.iter().any(|param| {
-        if let syn::GenericParam::Type(type_param) = param {
+    let has_t_generic = modified_fn.sig.generics.params.iter().any(|param|
+	{
+        if let syn::GenericParam::Type(type_param) = param
+		{
             type_param.ident == "T"
         } else {
             false
@@ -115,13 +118,15 @@ pub fn add_show(_args: TokenStream, input: TokenStream) -> TokenStream {
     });
     
     // Add the new generic parameter T only if it doesn't exist
-    if !has_t_generic {
+    if !has_t_generic
+	{
         let generic_param: syn::GenericParam = parse_quote! { T };
         modified_fn.sig.generics.params.push(generic_param);
         
         // Add the where clause for T: AsRef<[char]>
         let where_predicate: syn::WherePredicate = parse_quote! { T: AsRef<[char]> };
-        if modified_fn.sig.generics.where_clause.is_none() {
+        if modified_fn.sig.generics.where_clause.is_none()
+		{
             modified_fn.sig.generics.where_clause = Some(parse_quote! { where });
         }
         let where_clause = modified_fn.sig.generics.where_clause.as_mut().unwrap();
@@ -129,9 +134,12 @@ pub fn add_show(_args: TokenStream, input: TokenStream) -> TokenStream {
     }
     
     // Check if 'map' parameter already exists
-    let has_map_param = modified_fn.sig.inputs.iter().any(|arg| {
-        if let FnArg::Typed(pat_type) = arg {
-            if let syn::Pat::Ident(pat_ident) = &*pat_type.pat {
+    let has_map_param = modified_fn.sig.inputs.iter().any(|arg|
+	{
+        if let FnArg::Typed(pat_type) = arg
+		{
+            if let syn::Pat::Ident(pat_ident) = &*pat_type.pat
+			{
                 pat_ident.ident == "map"
             } else {
                 false
@@ -142,7 +150,8 @@ pub fn add_show(_args: TokenStream, input: TokenStream) -> TokenStream {
     });
     
     // Add the new parameter: map: &[T] only if it doesn't exist
-    if !has_map_param {
+    if !has_map_param
+	{
         let new_param: FnArg = parse_quote! { map: &[T] };
         modified_fn.sig.inputs.push(new_param);
     }
@@ -152,7 +161,8 @@ pub fn add_show(_args: TokenStream, input: TokenStream) -> TokenStream {
     replacer.visit_item_fn_mut(&mut modified_fn);
     
     // Generate the output containing both functions
-    let expanded = quote! {
+    let expanded = quote!
+	{
         #original_fn
         #modified_fn
     };
@@ -166,12 +176,18 @@ pub fn add_show(_args: TokenStream, input: TokenStream) -> TokenStream {
 /// Visitor to remove insert_here! macro calls entirely
 struct InsertHereRemover;
 
-impl VisitMut for InsertHereRemover {
-    fn visit_stmt_mut(&mut self, stmt: &mut Stmt) {
-        match stmt {
-            Stmt::Expr(expr, _semi_token) => {
-                if let Expr::Macro(macro_expr) = expr {
-                    if macro_expr.mac.path.is_ident("insert_here") {
+impl VisitMut for InsertHereRemover
+{
+    fn visit_stmt_mut(&mut self, stmt: &mut Stmt)
+	{
+        match stmt
+		{
+            Stmt::Expr(expr, _semi_token) =>
+			{
+                if let Expr::Macro(macro_expr) = expr
+				{
+                    if macro_expr.mac.path.is_ident("insert_here")
+					{
                         // Replace with an empty block expression
                         *expr = parse_quote! { {} };
                         return;
@@ -179,23 +195,29 @@ impl VisitMut for InsertHereRemover {
                 }
                 self.visit_expr_mut(expr);
             }
-            Stmt::Macro(macro_stmt) => {
-                if macro_stmt.mac.path.is_ident("insert_here") {
+            Stmt::Macro(macro_stmt) =>
+			{
+                if macro_stmt.mac.path.is_ident("insert_here")
+				{
                     // Replace the entire statement with an empty statement
                     *stmt = parse_quote! { {} };
                     return;
                 }
                 syn::visit_mut::visit_stmt_mut(self, stmt);
             }
-            _ => {
+            _ =>
+			{
                 syn::visit_mut::visit_stmt_mut(self, stmt);
             }
         }
     }
 
-    fn visit_expr_mut(&mut self, expr: &mut Expr) {
-        if let Expr::Macro(macro_expr) = expr {
-            if macro_expr.mac.path.is_ident("insert_here") {
+    fn visit_expr_mut(&mut self, expr: &mut Expr)
+	{
+        if let Expr::Macro(macro_expr) = expr
+		{
+            if macro_expr.mac.path.is_ident("insert_here")
+			{
                 // Replace with unit expression
                 *expr = parse_quote! { () };
                 return;
@@ -208,33 +230,42 @@ impl VisitMut for InsertHereRemover {
 /// Visitor to replace insert_here!(code) macro calls with the code inside them
 struct InsertHereReplacer;
 
-impl VisitMut for InsertHereReplacer {
-    fn visit_block_mut(&mut self, block: &mut Block) {
+impl VisitMut for InsertHereReplacer
+{
+    fn visit_block_mut(&mut self, block: &mut Block)
+	{
         // Process each statement in the block
-        for stmt in &mut block.stmts {
+        for stmt in &mut block.stmts
+		{
             self.visit_stmt_mut(stmt);
         }
     }
 
-    fn visit_stmt_mut(&mut self, stmt: &mut Stmt) {
-        eprintln!("Visiting statement type");
+    fn visit_stmt_mut(&mut self, stmt: &mut Stmt)
+	{
+        //eprintln!("Visiting statement type");
         
-        match stmt {
-            Stmt::Expr(expr, semi_token) => {
-                eprintln!("Found Stmt::Expr, has_semi: {}", semi_token.is_some());
-                if let Expr::Macro(macro_expr) = expr {
-                    eprintln!("Found macro in Stmt::Expr: {}", macro_expr.mac.path.get_ident().map_or("none".to_string(), |i| i.to_string()));
-                    if macro_expr.mac.path.is_ident("insert_here") {
-                        eprintln!("Found insert_here! in Stmt::Expr context");
+        match stmt
+		{
+            Stmt::Expr(expr, _semi_token) =>
+			{
+                //eprintln!("Found Stmt::Expr, has_semi: {}", semi_token.is_some());
+                if let Expr::Macro(macro_expr) = expr
+				{
+                    //eprintln!("Found macro in Stmt::Expr: {}", macro_expr.mac.path.get_ident().map_or("none".to_string(), |i| i.to_string()));
+                    if macro_expr.mac.path.is_ident("insert_here")
+					{
+                        //eprintln!("Found insert_here! in Stmt::Expr context");
                         let tokens = &macro_expr.mac.tokens;
-                        eprintln!("Tokens: {}", tokens);
+                        //eprintln!("Tokens: {}", tokens);
                         
                         // Try to parse the content as an expression
-                        if let Ok(replacement_expr) = syn::parse2::<Expr>(tokens.clone()) {
-                            eprintln!("Successfully parsed replacement expression");
+                        if let Ok(replacement_expr) = syn::parse2::<Expr>(tokens.clone())
+						{
+                            //eprintln!("Successfully parsed replacement expression");
                             *expr = replacement_expr;
                         } else {
-                            eprintln!("Failed to parse replacement expression");
+                            //eprintln!("Failed to parse replacement expression");
                         }
                         return; // Don't visit recursively if we found insert_here!
                     }
@@ -242,36 +273,42 @@ impl VisitMut for InsertHereReplacer {
                 // Visit the expression recursively if it's not insert_here!
                 self.visit_expr_mut(expr);
             }
-            Stmt::Local(_) => {
-                eprintln!("Found Stmt::Local (let binding)");
+            Stmt::Local(_) =>
+			{
+                //eprintln!("Found Stmt::Local (let binding)");
                 syn::visit_mut::visit_stmt_mut(self, stmt);
             }
-            Stmt::Item(_) => {
-                eprintln!("Found Stmt::Item");
+            Stmt::Item(_) =>
+			{
+                //eprintln!("Found Stmt::Item");
                 syn::visit_mut::visit_stmt_mut(self, stmt);
             }
-            Stmt::Macro(macro_stmt) => {
-                eprintln!("Found Stmt::Macro: {}", macro_stmt.mac.path.get_ident().map_or("none".to_string(), |i| i.to_string()));
-                if macro_stmt.mac.path.is_ident("insert_here") {
-                    eprintln!("Found insert_here! in Stmt::Macro context");
+            Stmt::Macro(macro_stmt) =>
+			{
+                //eprintln!("Found Stmt::Macro: {}", macro_stmt.mac.path.get_ident().map_or("none".to_string(), |i| i.to_string()));
+                if macro_stmt.mac.path.is_ident("insert_here")
+				{
+                    //eprintln!("Found insert_here! in Stmt::Macro context");
                     let tokens = &macro_stmt.mac.tokens;
-                    eprintln!("Tokens: {}", tokens);
+                    //eprintln!("Tokens: {}", tokens);
                     
                     // First try to parse as a statement (handles cases with semicolons inside)
-                    if let Ok(replacement_stmt) = syn::parse2::<Stmt>(tokens.clone()) {
-                        eprintln!("Successfully parsed replacement as statement");
+                    if let Ok(replacement_stmt) = syn::parse2::<Stmt>(tokens.clone())
+					{
+                        //eprintln!("Successfully parsed replacement as statement");
                         *stmt = replacement_stmt;
                         return;
                     }
                     
                     // If that fails, try to parse as an expression and convert to Stmt::Expr
-                    if let Ok(replacement_expr) = syn::parse2::<Expr>(tokens.clone()) {
-                        eprintln!("Successfully parsed replacement as expression");
+                    if let Ok(replacement_expr) = syn::parse2::<Expr>(tokens.clone())
+					{
+                        //eprintln!("Successfully parsed replacement as expression");
                         *stmt = Stmt::Expr(replacement_expr, macro_stmt.semi_token.clone());
                         return;
                     }
                     
-                    eprintln!("Failed to parse replacement content");
+                    //eprintln!("Failed to parse replacement content");
                     return;
                 }
                 syn::visit_mut::visit_stmt_mut(self, stmt);
@@ -279,13 +316,17 @@ impl VisitMut for InsertHereReplacer {
         }
     }
 
-    fn visit_expr_mut(&mut self, expr: &mut Expr) {
-        if let Expr::Macro(macro_expr) = expr {
-            if macro_expr.mac.path.is_ident("insert_here") {
+    fn visit_expr_mut(&mut self, expr: &mut Expr)
+	{
+        if let Expr::Macro(macro_expr) = expr
+		{
+            if macro_expr.mac.path.is_ident("insert_here")
+			{
                 let tokens = &macro_expr.mac.tokens;
                 
                 // Try to parse the content as an expression
-                if let Ok(replacement_expr) = syn::parse2::<Expr>(tokens.clone()) {
+                if let Ok(replacement_expr) = syn::parse2::<Expr>(tokens.clone())
+				{
                     *expr = replacement_expr;
                     return; // Don't visit recursively
                 }
