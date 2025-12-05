55a50975dd44cdf2690ce01dd86895d354c98ec8
Bloem <sem@ie-design.nl>
2025-12-05 14:55:47 +0100

    restructured the code, and added timers

--- 2025/05.rs
+++ 2025/05.rs
@@ -1,20 +1,59 @@
 use std::{
 	cmp::Ord,
-	ops::RangeInclusive
+	ops::RangeInclusive,
+	time::Instant,
 };
 
 
 pub fn setup(year: &str, day: &str)
 {
-	let file: Vec<String> = aoc::read_file(year, day, aoc::WhichFile::Main);
-	let _ = part1(&file);
-	let _ = part2(&file);
+	let time_file = Instant::now();
+	let mut answer_p1: i64 = 0;
+	let mut answer_p2: i64 = 0;
+	let file: Vec<String> = aoc::read_file(year, day, aoc::WhichFile::Test(2));
+	let time_used_file = time_file.elapsed();
+
+	let time_ranges = Instant::now();
+	let (start_id, ranges) = get_ranges(&file);
+	let time_used_ranges= time_ranges.elapsed();
+
+
+	let time_p1 = Instant::now();
+	'floop: for f in &file[start_id..]
+	{
+		for r in &ranges
+		{
+			if r.contains(&f.parse::<i64>().unwrap())
+			{
+				answer_p1 += 1;
+				continue 'floop;
+			}
+		}
+	}
+	let time_used_p1 = time_p1.elapsed();
+
+	let time_p2 = Instant::now();
+	for a in &ranges
+	{
+		answer_p2 += a.end() - a.start() + 1;
+	}
+	let time_used_p2 = time_p2.elapsed();
+	let time_used_total = time_file.elapsed();
+
+	println!("File:   {:5}ms", time_used_file.as_millis());
+	println!("Ranges: {:5}ms", time_used_ranges.as_millis());
+	println!("Part 1: {:5}ms", time_used_p1.as_millis());
+	println!("Part 2: {:5}ms", time_used_p2.as_millis());
+	println!("Total:  {:5}ms", time_used_total.as_millis());
+
+	println!();
+
+	println!("Part 1: {}", answer_p1);
+	println!("Part 2: {}", answer_p2);
 }
 
-fn part1(file: &Vec<String>)
+fn get_ranges(file: &Vec<String>) -> (usize, Vec<RangeInclusive<i64>>)
 {
-	let mut answer: i64 = 0;
-
 	let mut highest_id: i64 = 0;
 	let mut ranges: Vec<RangeInclusive<i64>> = Vec::new();
 	let mut end_ranges = 0;
@@ -46,65 +85,5 @@ fn part1(file: &Vec<String>)
 		}
 		allowed.push(r.clone());
 	}
-
-	'floop: for f in &file[end_ranges+1..]
-	{
-		for r in &allowed
-		{
-			if r.contains(&f.parse::<i64>().unwrap())
-			{
-				answer += 1;
-				continue 'floop;
-			}
-		}
-	}
-
-
-	println!("Part 1: {}", answer);
+	return (end_ranges + 1, allowed);
 }
-
-fn part2(file: &Vec<String>)
-{
-	let mut answer: i64 = 0;
-
-	let mut highest_id: i64 = 0;
-	let mut ranges: Vec<RangeInclusive<i64>> = Vec::new();
-
-	for f in file
-	{
-		if f.len() == 0
-		{
-			break;
-		}
-		let s: Vec<&str> = f.split("-").collect();
-		ranges.push(s[0].parse().unwrap()..=s[1].parse().unwrap());
-		highest_id = i64::max(highest_id, s[1].parse().unwrap())
-	}
-
-	ranges.sort_by(|a, b| i64::cmp(a.start(), b.start()));
-
-	let mut allowed: Vec<RangeInclusive<i64>> = vec![ranges[0].clone()];
-
-
-	for r in &ranges[1..]
-	{
-		let last: &mut std::ops::RangeInclusive<i64> = allowed.last_mut().unwrap();
-		if *r.start() <= *last.end()
-		{
-			let new_end = i64::max(*last.end(),*r.end());
-			*last = *last.start()..=new_end;
-			continue;
-		}
-		allowed.push(r.clone());
-	}
-
-	for a in &allowed
-	{
-		answer += a.end() - a.start() + 1;
-	}
-
-
-
-	println!("Part 2: {}", answer);
-}
-
